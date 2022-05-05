8dc4a188793a3287c6770c80a3348bd4ee162552
Florian <92161528+peflor@users.noreply.github.com>
2022-05-05 10:24:32 +0200

    V1
    tableau en dur et affichage nbr de personnes et noms des personnes ayant des lunettes

--- index.php
+++ index.php
@@ -1,24 +1,36 @@
 <?php
 
-$users = [  ["name" => "Adèle", "lunettes" => true],
-            ["name" => "Aurélie", "lunettes" => false],
-            ["name" => "Antoine", "lunettes" => true],
-            ["name" => "Bernard", "lunettes" => false],
-            ["name" => "Béatrice", "lunettes" => false],
-            ["name" => "Cédric", "lunettes" => false],
-            ["name" => "Charles", "lunettes" => true],
-            ["name" => "Claude", "lunettes" => false],
 
+$classMars = [
+    ["name" => "Florian" , "lunettes" => false],
+    ["name" => "Emmanuel" , "lunettes" => true],
+    ["name" => "Hakim" , "lunettes" => false],
+    ["name" => "Tristan" , "lunettes" => false],
+    ["name" => "Anis" , "lunettes" => false],
+    ["name" => "Laurent" , "lunettes" => false],
+    ["name" => "Anthony" , "lunettes" => true],
+    ["name" => "Nathan" , "lunettes" => false],
+    ["name" => "Kevin" , "lunettes" => false],
+    ["name" => "William" , "lunettes" => false],
+    ["name" => "Daouda" , "lunettes" => false],
+    ["name" => "Hajara" , "lunettes" => false],
+    ["name" => "Soufiane" , "lunettes" => false],
+    ["name" => "Christopher" , "lunettes" => true],
 ];
 
-var_dump ($users);
+$userLunette = [];
 
-foreach ($users as $user) {
-    // echo $user["name"]." ".$user["lunettes"];
-    if($user["lunettes"]==true){        
-        echo $user["name"]."<br>";
-        $userLunette = $user["name"];
-        var_dump($userLunette);
+    foreach ($classMars as $user) {    
+        if($user["lunettes"]==true){      
+            array_push($userLunette,$user["name"]);               
+        }
     }
-}
+
+echo "<p> Nombre de personne portant des lunettes : " .count($userLunette). "<br>Voici les noms : ".implode(", ",$userLunette)."</p>";
+
+
+
+
+
+
 
